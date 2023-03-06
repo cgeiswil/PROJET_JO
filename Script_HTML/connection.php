8ebb3b094813d8a3b5450109ddec54e0f07e57a0
Cohen Salmon Arthur <arthurcohensalmon0@gmail.com>
2023-03-06 18:13:32 +0100

    Ajout page connection (non finie) et la page Cquoi les JO (finie)

---
+++ Script_HTML/connection.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title> Connection </title>
+		<link rel="stylesheet" href="test.css" type="text/css">
+	</head>
+	<body>
+
+		<?php
+
+		echo "<form action='Accueil.html' method='post' autocomplete='off'>";
+
+		echo "Nom utilisateur/pseudo <INPUT type='text' name = 'pseudo' value = ''> <br> ";
+		echo "Mot de passe <INPUT type='text' name = 'mdp' value = ''><br>";
+		echo "<INPUT type='submit'  value = 'Envoyer'>";
+
+
+		echo "</form>";
+
+		?>
+
+ 
+
+	</body>
+</html>
