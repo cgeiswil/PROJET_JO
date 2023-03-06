8b3173a484ac479e3d2d9b3566ed757f9c27dea8
Cohen Salmon Arthur <arthurcohensalmon0@gmail.com>
2023-03-06 19:04:52 +0100

    Ajout des fichiers php ils ne sont pas encore finis

--- Script_HTML/connection.php
+++ Script_HTML/connection.php
@@ -9,7 +9,11 @@
 
 		<?php
 
-		echo "<form action='Accueil.html' method='post' autocomplete='off'>";
+		echo "Si vous n'avez pas encore créé votre compte, vous pouvez en créer un ici : <a href = nouveau.php> Nouveau utilisateur </a> ";
+
+		echo "</br>";
+
+		echo "<form action='enConnection.php' method='post' autocomplete='off'>";
 
 		echo "Nom utilisateur/pseudo <INPUT type='text' name = 'pseudo' value = ''> <br> ";
 		echo "Mot de passe <INPUT type='text' name = 'mdp' value = ''><br>";
