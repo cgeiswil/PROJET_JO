b646e47ed16a1a34822e5b37b1941a8017f32319
Malo <malo.aityahia@gmail.com>
2023-03-27 00:02:13 +0200

    version php

---
+++ Script_HTML/Mentions_legales.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title>Mentions Légales</title>
+        <meta charset="utf-8">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+        <link rel="stylesheet" href ="Styles/Mentions_legales_css.css" type = "text/css" media = "screen">
+		<link rel="shortcut icon" href="../Images/Anneaux/officiel.png" type="image/png">
+    </head>
+    <body>
+    
+    <object data="Barre_de_navigation.html" width="100%" height="100%">
+	</object>
+<div class="mention">
+<h1>MENTIONS LÉGALES</h1>
+<h2>Informations légales</h2>
+<p>Ce site a été réalisé dans le cadre d'un travail universitaire de troisième année en licence Miashs à l'université Paul Valéry encadré par l'enseignement de l'ECUE TE5PR1MI (S5) et TE64MI (S6).<br>
+<u>Coordonnées de l'université :</u><br>
+	Adresse : Université Paul Valéry Montpellier 3, Route de Mende, 34199 Montpellier Cedex 5.<br>
+	Standard : Tél. : <a href="tel:+33467142000">+33 (0)4 67 14 20 00</a><br><br>
+​​
+	Toutefois, la création du site et sa maintenance est assurée par l'équipe du projet <em>« Du retour des Jeux Olympiques, aux plus grands athlètes de leurs disciplines »</em>.</p>
+
+
+<p><u>Coordonnées de l'équipe :</u>
+<ul>
+	<li>Malo AIT YAHIA : <a href="mailto:malo.ait-yahia@etu.univ-montp3.fr">malo.ait-yahia@etu.univ-montp3.fr</a></li>
+	<li>Thomas AYRIVIÉ : <a href="mailto:thomas.ayrivie@etu.univ-montp3.fr">thomas.ayrivie@etu.univ-montp3.fr</a></li>
+	<li>Arthur COHEN SALMON : <a href="mailto:arthur.cohen-salmon@etu.univ-montp3.fr">arthur.cohen-salmon@etu.univ-montp3.fr</a></li>
+	<li>Candice DÉJEAN : <a href="mailto:candice.dejean@etu.univ-montp3.fr">candice.dejean@etu.univ-montp3.fr</a></li>
+	<li>Christelle GEISWILLER : <a href="mailto:christelle.geiswiller@etu.univ-montp3.fr">christelle.geiswiller@etu.univ-montp3.fr</a></li>
+	<li>Alex ROLAND : <a href="mailto:alex.roland@etu.univ-montp3.fr">alex.roland@etu.univ-montp3.fr</a></p></li>
+​</ul>
+
+<h2>Données personnelles et cookies</h2>
+<p>Sur le présent site, les données personnelles ne sont pas récoltées, utilisées et prises à votre insu, 
+quelque soit la manière. Aucun cookies n'est déposé sur votre ordinateur, autrement, votre concentement serait demandé.</p>
+​
+<h2>Reproduction et réutilisation</h2>
+<p>Les données présentes sur le site peuvent être réutilisées, à condition du respect de l’intégrité des documents/données reproduites : 
+pas de modification, et de l'ajout de façon claire et lisible de la source. 
+Le travail venant de personnes extérieures à notre équipe, les images, les données, les documents, les citations ne venant pas de notre propre travail sont citées,
+ selon le code de la propriété intellectuelle, parfois en-dessous du dit travail, par renvoi, ou sur une page de bibiliographie -ce travail étant un travail universitaire-.</p>
+​
+<h2>Décharges et juridiction.</h2>
+<p>L'équipe décharge toutes reponsabilités, aux potentielles conséquences liées aux défaillances techniques en relation avec le site. 
+De plus, le contenu des pages est purement informatif et ne serait avoir de valeur contractuelle. 
+Le site et les contenus sont créés conformément aux règles juridiques applicables en France, même s'ils sont accessibles à des personnes venant d'autres pays. 
+Tous litiges sont d'abord traités à l'amiable via nos contacts ci-avant, puis le cas échéant aux tribunaux français. 
+Nous considérons que tous les visiteurs doivent avoir pris connaissance de cette page, avant de pouvoir consulter le reste du site.
+Dans tous les cas, en poursuivant la navigation, ils donnent leurs accords implicites aux propos contenus sur cette présente page.</p>
+</div>
+<br><br>
+
+<footer>
+	<object  data="Pied_de_page.php" width="100%" height="100%">
+	</object>
+</footer>
+</body>
+</html>
