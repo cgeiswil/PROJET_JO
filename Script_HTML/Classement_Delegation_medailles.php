90a1d6480b53a748613cf285dccb51f4cd155558
Malo <malo.aityahia@gmail.com>
2023-03-26 15:16:39 +0200

    nouvelle version

--- Script_HTML/Classement_Delegation_medailles.php
+++ Script_HTML/Classement_Delegation_medailles.php
@@ -58,7 +58,14 @@ require("fonction.php");
 $bd = getBDD();
 
 
-$requete_or = $bd -> query("select requete_imbriquee.id_pays, count(requete_imbriquee.nb) as nb_medailles, requete_imbriquee.nom_pays, requete_imbriquee.I_drapeau as drapeau
+
+
+$couleur_medaille = array("Gold","Silver","Bronze");
+$lignes = array();
+$taille_classement = "5";
+
+for ( $i = 0  ; $i < 3; $i++){
+$requete = $bd -> query("select requete_imbriquee.id_pays, count(requete_imbriquee.nb) as nb_medailles, requete_imbriquee.nom_pays, requete_imbriquee.I_drapeau as drapeau
 from (
         select etre_nationalite.id_olympiade, etre_nationalite.id_pays, count(DISTINCT lier_m.id_epreuves) as nb, pays_participants.nom_pays, pays_participants.I_drapeau
         from athletes,lier_m,medailles,pays_participants,etre_nationalite,olympiades,epreuves
@@ -68,59 +75,21 @@ from (
                 etre_nationalite.id_pays = pays_participants.Code_CIO
                 and lier_m.id_olympiade = olympiades.id_olympiade
                 and epreuves.id_epreuves = lier_m.id_epreuves 
-                and medailles.type = 'Gold'
+                  and medailles.type = '".$couleur_medaille[$i]."'
          and lier_m.id_olympiade = etre_nationalite.id_olympiade
                 group by etre_nationalite.id_olympiade, etre_nationalite.id_pays, lier_m.id_epreuves
         ORDER BY etre_nationalite.id_pays ASC
 ) as requete_imbriquee
  group by requete_imbriquee.id_pays 
 order by  nb_medailles  DESC 
-limit 4");
+limit ".$taille_classement);
 
-$requete_argent = $bd -> query("select requete_imbriquee.id_pays, count(requete_imbriquee.nb) as nb_medailles, requete_imbriquee.nom_pays, requete_imbriquee.I_drapeau as drapeau
-from (
-        select etre_nationalite.id_olympiade, etre_nationalite.id_pays, count(DISTINCT lier_m.id_epreuves) as nb, pays_participants.nom_pays, pays_participants.I_drapeau
-        from athletes,lier_m,medailles,pays_participants,etre_nationalite,olympiades,epreuves
-                where athletes.ID_athletes = lier_m.ID_athletes
-                and lier_m.id_medaille = medailles.id_medaille
-                and athletes.ID_athletes = etre_nationalite.ID_athletes and
-                etre_nationalite.id_pays = pays_participants.Code_CIO
-                and lier_m.id_olympiade = olympiades.id_olympiade
-                and epreuves.id_epreuves = lier_m.id_epreuves 
-                and medailles.type = 'Silver'
-         and lier_m.id_olympiade = etre_nationalite.id_olympiade
-                group by etre_nationalite.id_olympiade, etre_nationalite.id_pays, lier_m.id_epreuves
-        ORDER BY etre_nationalite.id_pays ASC
-) as requete_imbriquee
- group by requete_imbriquee.id_pays 
-order by  nb_medailles  DESC 
-limit 4");
 
-$requete_bronze = $bd -> query("select requete_imbriquee.id_pays, count(requete_imbriquee.nb) as nb_medailles, requete_imbriquee.nom_pays, requete_imbriquee.I_drapeau as drapeau
-from (
-        select etre_nationalite.id_olympiade, etre_nationalite.id_pays, count(DISTINCT lier_m.id_epreuves) as nb, pays_participants.nom_pays, pays_participants.I_drapeau
-        from athletes,lier_m,medailles,pays_participants,etre_nationalite,olympiades,epreuves
-                where athletes.ID_athletes = lier_m.ID_athletes
-                and lier_m.id_medaille = medailles.id_medaille
-                and athletes.ID_athletes = etre_nationalite.ID_athletes and
-                etre_nationalite.id_pays = pays_participants.Code_CIO
-                and lier_m.id_olympiade = olympiades.id_olympiade
-                and epreuves.id_epreuves = lier_m.id_epreuves 
-                and medailles.type = 'Bronze'
-         and lier_m.id_olympiade = etre_nationalite.id_olympiade
-                group by etre_nationalite.id_olympiade, etre_nationalite.id_pays, lier_m.id_epreuves
-        ORDER BY etre_nationalite.id_pays ASC
-) as requete_imbriquee
- group by requete_imbriquee.id_pays 
-order by  nb_medailles  DESC 
-limit 4");
+$lignes[$i]  =  $requete -> fetchall(); 
+}
 
 
 
-$lignes_or = $requete_or -> fetchAll();
-$lignes_argent = $requete_argent-> fetchAll();
-$lignes_bronze = $requete_bronze -> fetchAll();
-        
 
          
 ?>
@@ -137,9 +106,9 @@ $lignes_bronze = $requete_bronze -> fetchAll();
      <table  id = "troisdrapeaux">
 
      <tr>
-     <td class = " celluletab" > <img class = "drapeaufr"  src = <?= $lignes_or[0]["drapeau"] ?> alt="Drapeau premier" class="img-thumbnail border-0" width="40px"> </td>
-     <td class = " celluletab" > <img class = "drapeaufr"  src=  <?= $lignes_or[1]["drapeau"] ?> alt="Drapeau deuxieme" class="img-thumbnail border-0" width="40px"> </td>
-     <td class = " celluletab" > <img class = "drapeaufr"  src=  <?= $lignes_or[2]["drapeau"] ?> alt="Drapeau troisieme" class="img-thumbnail border-0" width="40px"> </td>
+     <td class = " celluletab" > <img class = "drapeaufr"  src = <?= $lignes[0][0]["drapeau"] ?> alt="Drapeau premier" class="img-thumbnail border-0" width="40px"> </td>
+     <td class = " celluletab" > <img class = "drapeaufr"  src=  <?= $lignes[0][1]["drapeau"] ?> alt="Drapeau deuxieme" class="img-thumbnail border-0" width="40px"> </td>
+     <td class = " celluletab" > <img class = "drapeaufr"  src=  <?=$lignes[0][2]["drapeau"] ?> alt="Drapeau troisieme" class="img-thumbnail border-0" width="40px"> </td>
 
     </tr>
 
@@ -149,11 +118,6 @@ $lignes_bronze = $requete_bronze -> fetchAll();
      <td class = " celluletab" > <img class = "drapeaufr"  src = "../Images/Boutons/medaille_bronze.png" alt="medaille bronze" class="img-thumbnail border-0" width="40px"> </td>
     </tr>
 
-     
-
-
-
-
      </table>
     
      
@@ -161,7 +125,7 @@ $lignes_bronze = $requete_bronze -> fetchAll();
      <div class="container card">
      <div class="card-body">
                   <h5 class="card-title">Classement officiel du Comité International Olympique</h5>
-                  <p class="card-text">Description du classement.</p>
+                  <p class="card-text">Classement en fonction du nombre de médailles d'or gagnées par les délégations.</p>
       <div class="row">
         <div class="col-12">
 
@@ -169,38 +133,28 @@ $lignes_bronze = $requete_bronze -> fetchAll();
         
             <table class="table">
             <tbody>
+              
 
-              <tr>
-                <td>1</td>
-                <th scope="row"><img class = "drapeaufr"  src= <?= $lignes_or[0]["drapeau"] ?> alt="Drapeau France" class="img-thumbnail border-0" width="40px"> <?= $lignes_or[0]["nom_pays"] ?></th>
-                <td> <img class = "imageclassement" src="../Images/Boutons/medaille_or.png" alt="Médaille d'or" width="20px"> <span> <?= $lignes_or[0]["nb_medailles"] ?></span></td>
-                <td><img class = "imageclassement"src="../Images/Boutons/medaille_argent.png" alt="Médaille d'argent" width="20px"> <span> <?= $lignes_argent[0]["nb_medailles"] ?></span></td>
-                <td><img class = "imageclassement" src="../Images/Boutons/medaille_bronze.png" alt="Médaille de bronze" width="20px"> <span><?= $lignes_bronze[0]["nb_medailles"] ?></span></td>
-                <td>= <?= $lignes_or[0]["nb_medailles"] + $lignes_argent[0]["nb_medailles"] +  $lignes_bronze[0]["nb_medailles"] ?></td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <th scope="row"><img class = "drapeaufr"  src=<?= $lignes_or[1]["drapeau"] ?> alt="Drapeau France" class="img-thumbnail border-0" width="40px"> <?= $lignes_or[1]["nom_pays"] ?></th>
-                <td><img  class = "imageclassement"src="../Images/Boutons/medaille_or.png" alt="Médaille d'or" width="20px"> <span><?= $lignes_or[1]["nb_medailles"] ?></span></td>
-                <td><img  class = "imageclassement" src="../Images/Boutons/medaille_argent.png" alt="Médaille d'argent" width="20px"> <span><?= $lignes_argent[1]["nb_medailles"] ?></span></td>
-                <td><img  class = "imageclassement" src="../Images/Boutons/medaille_bronze.png" alt="Médaille de bronze" width="20px"> <span><?= $lignes_bronze[1]["nb_medailles"] ?></span></td>
-                <td>= <?= $lignes_or[1]["nb_medailles"] + $lignes_argent[1]["nb_medailles"] +  $lignes_bronze[1]["nb_medailles"]; ?></td>
-              </tr>
-                        <tr>
-                <td>3</td>
-                <th scope="row"> <img class = "drapeaufr" src= <?= $lignes_or[2]["drapeau"] ?> alt="Drapeau France" class="img-thumbnail border-0" width="40px"><?= $lignes_or[2]["nom_pays"] ?> </th>
-                <td><img class = "imageclassement" src="../Images/Boutons/medaille_or.png" alt="Médaille d'or" width="20px"> <span><?= $lignes_or[2]["nb_medailles"] ?></span></td>
-                <td><img class = "imageclassement" src="../Images/Boutons/medaille_argent.png" alt="Médaille d'argent" width="20px"> <span><?= $lignes_argent[2]["nb_medailles"] ?></span></td>
-                <td><img class = "imageclassement"  src="../Images/Boutons/medaille_bronze.png" alt="Médaille de bronze" width="20px"> <span><?= $lignes_bronze[2]["nb_medailles"] ?></span></td>
-                <td>= <?=   $lignes_or[2]["nb_medailles"] + $lignes_argent[2]["nb_medailles"] +  $lignes_bronze[2]["nb_medailles"]; ?> </td>
-              <tr>
-                <td>4</td>
-                <th scope="row"><img class = "drapeaufr" src=<?= $lignes_or[3]["drapeau"] ?> alt="Drapeau France" class="img-thumbnail border-0" width="40px"><?= $lignes_or[3]["nom_pays"] ?></th>
-                <td><img  class = "imageclassement" src="../Images/Boutons/medaille_or.png" alt="Médaille d'or" width="20px"> <span><?= $lignes_or[3]["nb_medailles"] ?></span></td>
-                <td><img  class = "imageclassement" src="../Images/Boutons/medaille_argent.png" alt="Médaille d'argent" width="20px"> <span><?= $lignes_argent[3]["nb_medailles"] ?></span></td>
-                <td><img class = "imageclassement"  src="../Images/Boutons/medaille_bronze.png" alt="Médaille de bronze" width="20px"> <span><?= $lignes_bronze[3]["nb_medailles"] ?></span></td>
-                <td>= <?=  $lignes_or[3]["nb_medailles"] + $lignes_argent[3]["nb_medailles"] +  $lignes_bronze[3]["nb_medailles"]; ?> </td>
-              </tr>
+              <?php     
+
+              
+    $i = 0;
+
+    while ($i < $taille_classement) {
+
+        echo "<tr>
+                  <td>".strval($i + 1)."</td>
+                  <th scope='row'><img class='drapeaufr' src='" . $lignes[0][$i]["drapeau"] . "' alt='Drapeau France' class='img-thumbnail border-0' width='40px'>" . $lignes[0][$i]["nom_pays"] . "</th>
+                  <td><img class='imageclassement' src='../Images/Boutons/medaille_or.png' alt='Médaille d'or' width='20px'> <span>" . $lignes[0][$i]["nb_medailles"] . "</span></td>
+                  <td><img class='imageclassement' src='../Images/Boutons/medaille_argent.png' alt='Médaille d'argent' width='20px'> <span>" . $lignes[1][$i]["nb_medailles"] . "</span></td>
+                  <td><img class='imageclassement' src='../Images/Boutons/medaille_bronze.png' alt='Médaille de bronze' width='20px'> <span>" . $lignes[2][$i]["nb_medailles"] . "</span></td>
+                  <td>= " . ($lignes[0][$i]["nb_medailles"] + $lignes[1][$i]["nb_medailles"] + $lignes[2][$i]["nb_medailles"]) . "</td>
+              </tr>";
+
+        $i += 1;
+    }
+?>
+
 
               </tbody>
           </table>
