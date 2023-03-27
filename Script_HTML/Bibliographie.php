570f54fe61d01c9e4b2eb2a9aa24305d1d8f943a
Malo <malo.aityahia@gmail.com>
2023-03-27 23:39:25 +0200

    version php

---
+++ Script_HTML/Bibliographie.php
@@ -0,0 +1,111 @@
+<!DOCTYPE html>
+<html lang="fr">
+		<head>
+				<title>Bibliographie et ressources utilisées</title>
+				<meta charset="utf-8">
+				<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet">
+				<link rel="stylesheet" href ="Styles/Bibliographie.css" type = "text/css" media = "screen">
+				<link rel="shortcut icon" href="../Images/Anneaux/officiel.png" type="image/png">
+
+		
+           <style>
+
+			a{
+
+				color: blue;
+
+			}
+
+			.contenu{
+
+				padding-left: 100px;
+			}
+
+			</style>
+			
+		</head>
+		<body>
+   
+
+        
+		
+		<object data="Barre_de_navigation.html" width="100%" height="100%">
+	</object> 
+
+	<div><h1>Bibliographie <br>et ressources utilisées </h1>
+<p>Pour la réalisation de ce site, nous avons utilisé de nombreuses données et ressources. 
+Pour chacune d'entre-elles vous trouverez ci-après leurs auteurs et liens. 
+Nous souhaitons aussi remercier les personnes propriétaires de ses données de les avoir mises en accès libre.<br>
+<strong>Moins de 0,1% des données proviennent de base de données créées par nos moyens.
+<br>Le reste provient de bases de données déjà existantes. </strong></p>
+<br>
+    
+
+<br>
+	<table class ="main"  >
+				<tr > 
+				<td><img class="logo" src="../Images/Bibliographie/logo_donnees_biblio.png" alt="logo base de données"></td>
+				<td><p> <strong> Source de données 1 (Athlètes) </strong></p></td>
+				<td  class  = "contenu"><p>Données extraites d'une base de données open-source.<br>
+				  de 1896 à 2016 : <a href = "https://www.kaggle.com/datasets/heesoo37/120-years-of-olympic-history-athletes-and-results"> https://www.kaggle.com/datasets/heesoo37/120-years-of-olympic-history-athletes-and-results</a>
+                </tr>
+    
+			<tr>
+		<td><img class="logo" src="../Images/Bibliographie/logo_donnees_biblio.png" alt="logo base de données"></td>
+		<td><p> <strong> Source de données 2 </strong> </p></td>
+		<td  class  = "contenu">  Codes pays du CIO + populations :<a href =" https://www.kaggle.com/datasets/the-guardian/olympic-games"> https://www.kaggle.com/datasets/the-guardian/olympic-games</a>
+        <p> Codes du Comité International Olympique des délégations, 
+		drapeaux (Wikipédia), populations, PIB. Créée par complétion, grâce aux données disponibles sur wikipedia.orgs et olympics.com.</td>
+		</tr>
+
+
+			<tr>
+			<td><img class="logo" src="../Images/Bibliographie/logo_donnees_biblio.png" alt="logo base de données"></td>
+			<td><p> <strong> Source de données 3  </strong></p></td>
+			<td class  = "contenu"><p>Éditions des jeux olym-
+piques, logos, ville et pays hôte, dates et chiffres clés sur les téléspectateurs, le
+coût. Créée par complétion, grâce aux données disponibles sur wikipedia.org et
+olympics.com
+			</p></td>
+			</tr>
+
+
+
+			<tr>
+			<td><img class="logo" src="../Images/Bibliographie/logo_donnees_biblio.png" alt="logo base de données"></td>
+
+			<td><p> <strong>Source de données 4 </strong></p></td>
+			<td class  = "contenu"><p> Records olympiques délivrés par le CIO (Athlète, informations de situation et temporelles, record olympique). Créée par complétion, grâce aux données disponibles sur olympiandatabase.com et pour le cyclisme sur Wikipedia - Liste des records olympiques de cycisme sur piste. </p></td>
+			</tr>
+
+
+			<tr>
+			<td><img class="logo" src="../Images/Bibliographie/logo_donnees_biblio.png" alt="logo base de données"></td>
+
+			<td><p> <strong> Source de données 5 </strong></p></td>
+			<td  class  = "contenu"><p>   Anecdotes classées par mots
+                      clés sur les JO depuis l’antiquité. Créée par complétion grâce aux sources indiquées
+                      pour chaque ligne, dans une colonne de la feuille.</p></td>
+			</tr>
+
+			<tr>
+			<td><img class="logo" src="../Images/Bibliographie/logo_donnees_biblio.png" alt="logo base de données"></td>
+
+			<td><p><strong>Source de données 6 </strong></p></td>
+			<td class  = "contenu"><p>   Questions sur les jeux
+             olympiques, avec réponses et difficultés. Créée par complétion grâce aux sources
+             indiquées pour chaque ligne, dans une colonne de la feuille.</p></td>
+			</tr>
+
+
+
+
+				</table>
+				</div>
+				
+<footer>
+	<object  data="Pied_de_page.php" width="100%" height="100%">
+	</object>
+</footer>
+		</body>
+		</html>
