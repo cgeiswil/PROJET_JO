e349bd4983c14f172670a865338366187858e41b
Cohen Salmon Arthur <arthurcohensalmon0@gmail.com>
2023-04-04 14:14:32 +0200

    J'ai pratiquement terminé le formulaire de connection il me reste plus qu'à vérifier les pseudos. J'ai apporté quelques modifications à athlètes qu'en pensez vous ?

--- Script_HTML/Athletes.php
+++ Script_HTML/Athletes.php
@@ -98,32 +98,81 @@
 			  echo '<td>'.$AthMed['nb_medailles_or']+$AthMed['nb_medailles_Ar']+$AthMed['nb_medailles_Br'].'</td>';
 			  echo '</tr>';
 			  echo '</table>';
+			  echo '<br><br><br>';
 
 
-			  $Olymp = $bdd -> prepare('select * from olympiades, etre_nationalite, athletes, pays_participants
+			  $Olymp = $bdd -> prepare('select olympiades.id_olympiade, olympiades.annee_o, villes_hotes.nom as Nom, pays_participants.nom_pays, etre_nationalite.id_pays, athletes.nom, villes_hotes.latitude, olympiades.logo, villes_hotes.longitude from olympiades, etre_nationalite, athletes, pays_participants, villes_hotes
 				  where athletes.ID_athletes = ?
 				  and etre_nationalite.ID_athletes = athletes.ID_athletes
 				  and etre_nationalite.id_pays = pays_participants.Code_CIO
-				  and etre_nationalite.id_olympiade = olympiades.id_olympiade');
-			  $Olymp -> execute($_GET['id']);
+				  and etre_nationalite.id_olympiade = olympiades.id_olympiade
+				  and villes_hotes.id_ville = olympiades.id_ville_hote');
+			  $Olymp -> execute([$_GET['id']]);
 
 			  	echo '<h2 class="tableau"> Olympiades Particip&eacute;es </h2>';
 				echo '<table>';
 				echo '<tr>';
 				echo '<th> Ann&eacute;e</th>';
 				echo '<th> Ville h&ocirc;te </th>';
-				echo "<th> Nombre d'athletes</th>";
-				echo "<th> Nombre de discplines</th>";
-				echo "<th> Nombre d'&eacute;preuves</th>";
-				echo "<th> Nombre de d&eacute;l&eacute;gations</th>";
+				echo "<th> Pays repr&eacute;sent&eacute;s</th>";
+				echo "<th> Nombre de m&eacute;dailles du pays</th>";
+				echo "<th> Nombre de m&eacute;dailles de l'athl&egrave;tes</th>";
 				echo "<th> Logo de l'olympiade </th>";
 				echo '</tr>';
 
 			  while($OlympP = $Olymp -> fetch()){
+			  	$MedPays = $bdd -> prepare('select requete_imbriquee.id_pays, count(requete_imbriquee.nb) as nb_medailles, requete_imbriquee.nom_pays, requete_imbriquee.I_drapeau
+				FROM (
+				        select etre_nationalite.id_olympiade, etre_nationalite.id_pays, count(DISTINCT lier_m.id_epreuves) as nb, pays_participants.nom_pays, pays_participants.I_drapeau, disciplines.nom_discipline
+				        from athletes, lier_m, medailles, pays_participants, etre_nationalite, olympiades, epreuves, disciplines
+				                where athletes.ID_athletes = lier_m.ID_athletes
+				                and lier_m.id_medaille = medailles.id_medaille
+				                and athletes.ID_athletes = etre_nationalite.ID_athletes AND
+				                etre_nationalite.id_pays = pays_participants.Code_CIO
+				                and lier_m.id_olympiade = olympiades.id_olympiade
+				                and epreuves.id_epreuves = lier_m.id_epreuves 
+				                and epreuves.id_disciplines = disciplines.id_discipline
+				                and medailles.id_medaille != 4
+				         		and lier_m.id_olympiade = etre_nationalite.id_olympiade
+				    			and etre_nationalite.id_pays = ?
+				    			and olympiades.id_olympiade = ?
+				                group by etre_nationalite.id_olympiade, etre_nationalite.id_pays, lier_m.id_epreuves, disciplines.id_discipline
+				        ORDER BY `etre_nationalite`.`id_pays` ASC
+				) as requete_imbriquee
+				 group by requete_imbriquee.id_pays 
+				ORDER BY `nb_medailles`  DESC limit 5');
+			  	$MedPays -> execute([$OlympP['id_pays'], $OlympP['id_olympiade']]);
+			  	$MedP = $MedPays -> fetch();
 
+			  	$AthOlymp = $bdd -> prepare("select athletes.ID_athletes, athletes.nom, athletes.sexe,
+			    COUNT(CASE WHEN medailles.type = 'Gold' THEN 1 ELSE NULL END) AS nb_medailles_or, 
+			    COUNT(CASE WHEN medailles.type = 'Silver' THEN 1 ELSE NULL END) AS nb_medailles_Ar,
+			    COUNT(CASE WHEN medailles.type = 'Bronze' THEN 1 ELSE NULL END) AS nb_medailles_Br
+			FROM athletes, lier_m, medailles, olympiades 
+			where athletes.ID_athletes=lier_m.ID_athletes and lier_m.id_medaille=medailles.id_medaille 			   and athletes.ID_athletes = ? and olympiades.id_olympiade = lier_m.id_olympiade and 					olympiades.id_olympiade = ?
+			GROUP BY athletes.ID_athletes, athletes.nom, athletes.sexe
+			ORDER BY nb_medailles_or DESC, nb_medailles_Ar DESC, nb_medailles_Br");
+			  	$AthOlymp -> execute([$_GET['id'], $OlympP['id_olympiade']]);
+			  	$AthOMed = $AthOlymp -> fetch();
+			  	$Total = $AthOMed['nb_medailles_Br']+$AthOMed['nb_medailles_Ar']+$AthOMed['nb_medailles_or'];
+
+
+
+			  	echo '<tr>';
+			  	echo '<td> <a href="Edition_particuliere.php?id='.$OlympP['id_olympiade'].'">'.$OlympP['annee_o'].'</td>';
+			  	echo '<td>  <a href="Vision_par_editions.php?view=p&lat='.$OlympP['latitude'].'&lon='.$OlympP['longitude'].'#carte">'.$OlympP['Nom'].'</td>';
+			  	echo '<td>'.$OlympP['nom_pays'].'</td>';
+			    echo '<td>'.$MedP['nb_medailles'].'</td>';
+			    echo '<td>'.$Total.'</td>';
+			    echo '<td> <img src="'.$OlympP['logo'].'" alt="oups"></td>';
+			  	echo '</tr>';
 
 			  }
+
+			  echo '</table>';
 
 		
 
 		?>
+	</body>
+</html>
--- Script_HTML/enregistrement.php
+++ Script_HTML/enregistrement.php
@@ -22,6 +22,24 @@
 
 		}
 
+		$temp = $_POST['mail'];
+		$taille = strlen($temp);
+		$drap = false;
+		$i = 0;
+		while($i < $taille){
+			if($temp[$i] == '@'){
+				echo 'if<br>';
+				$drap = true;
+				$i = $taille;
+			}else{
+				
+				$i = $i+1;
+				$drap = false;
+
+			}
+			
+		}
+
 		if((isset($_POST['ps']) and empty($_POST['ps']))
 			or (isset($_POST['mail']) and empty($_POST['mail']))
 			or (isset($_POST['mdp']) and empty($_POST['mdp']))
@@ -32,74 +50,77 @@
 			echo "pas rempli";			
 		
 		}else{
-
-			if($_POST['mdp2'] == $_POST['mdp']){
+			if($drap){
+				echo 'drapeau true';
+				if($_POST['mdp2'] == $_POST['mdp']){
 				
-				//echo 'mdp egaux';
-				//$bdd = getBDD();
 				$verifMail = $bdd -> prepare('select email from utilisateurs where email= ?');
 				$mailPara = $_POST['mail'];
-				//echo $mailPara;
-				//echo 'preparee';
+				
 				$verifMail -> execute([$mailPara]);
-				//echo 'executee';
-				//$a = prepare('select email from utilisateurs where email= "?"');
+				
 				if($verifMail -> fetch()){
 					echo "adresse mail deja existente merci de la modifier ou de vous connecter avec le compte correspondant &agrave; l'adresse mail renseign&eacute;e";
-					echo '<meta http-equiv="refresh" content="3; url= nouveau.php>';
+					echo '<meta http-equiv="refresh" content="3; url= nouveau.php?n='.$_POST['ps'].'&mail='.$_POST['mail'].'">';
 
 				}else {
 
-				session_start();
-				echo '<meta http-equiv="refresh" content="10; url=Profil.php">';
+					session_start();
+					echo '<meta http-equiv="refresh" content="10; url=Profil.php">';
 
-				echo $_POST['ps']."</br>";
-				echo $_POST['mail']."</br>";
-				echo $_POST['mdp']."</br>";
-
-
-				enregistrer($_POST['ps'], $_POST['mail'], $_POST['mdp']);
-				
-				echo "voici les informations que vous avez rentrée, vous serez redirigé vers la page d'accueil d'ici quelques secondes"; 
-				echo "<br>";
-				echo "Pseudo : ".$_POST['ps'];
-				echo "<br>";
-				echo "Adresse mail : ".$_POST['mail'];
+					echo $_POST['ps']."</br>";
+					echo $_POST['mail']."</br>";
+					echo $_POST['mdp']."</br>";
 
 
-				$resultT = $bdd -> prepare('select * from utilisateurs where pseudo = ?');
+					enregistrer($_POST['ps'], $_POST['mail'], $_POST['mdp']);
+					
+					echo "voici les informations que vous avez rentrée, vous serez redirigé vers la page d'accueil d'ici quelques secondes"; 
+					echo "<br>";
+					echo "Pseudo : ".$_POST['ps'];
+					echo "<br>";
+					echo "Adresse mail : ".$_POST['mail'];
 
-				$resultT -> execute([$_POST['ps']]);
-				
-				$result = $resultT  -> fetch();
-				
-				$_SESSION['utilisateur'] = array(
-					'utilisateur' => $result['id_utilisateur'],
-					'pseudo' => $result['pseudo'],
-					'MDP' => $result['mot_de_passe'],
-					'email' => $result['email'],
-					'photo' => $result['photo'],
-					'nb_heures' => $result['nb_heures']);
 
-				echo "test";
-				echo $_SESSION['utilisateur']['utilisateur'];
+					$resultT = $bdd -> prepare('select * from utilisateurs where pseudo = ?');
 
-				}
+					$resultT -> execute([$_POST['ps']]);
+					
+					$result = $resultT  -> fetch();
+					
+					$_SESSION['utilisateur'] = array(
+						'utilisateur' => $result['id_utilisateur'],
+						'pseudo' => $result['pseudo'],
+						'MDP' => $result['mot_de_passe'],
+						'email' => $result['email'],
+						'photo' => $result['photo'],
+						'nb_heures' => $result['nb_heures']);
 
-				
-				
-				
+					echo "test";
+					echo $_SESSION['utilisateur']['utilisateur'];
+
+					}
+
+					
+					
+					
 
 
 			}else{
 
 				echo "Les mots de passe entrés ne sont pas les mêmes. Vous serez redirigés dans quelques instant";
 				echo "<br>";
-				echo $_POST['mdp2'];
 				echo '<meta http-equiv="refresh" content="3; url= nouveau.php?n='.$_POST['ps'].'&mail='.$_POST['mail'].'">';
 
 			}
+		}else{
+			echo 'drapeau false';
+			echo "L'adresse mail que vous avez entr&eacute;e ne contient pas de @. Elle n'est donc pas valide. Merci d'entrer une adresse mail valide.";
+			echo "<br>";
+			
+			echo '<meta http-equiv="refresh" content="3; url= nouveau.php?n='.$_POST['ps'].'&mail='.$_POST['mail'].'">';
 		}
+	}
 		?>
 	</head>
 	<body>
