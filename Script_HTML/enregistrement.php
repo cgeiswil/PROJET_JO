d539c92a3377bd4e91bbfb9b71e1fee77473f60e
Cohen Salmon Arthur <arthurcohensalmon0@gmail.com>
2023-04-04 14:44:06 +0200

    Formulaire de connection officiellement terminé.

--- Script_HTML/enregistrement.php
+++ Script_HTML/enregistrement.php
@@ -50,74 +50,78 @@
 			echo "pas rempli";			
 		
 		}else{
-			if($drap){
-				echo 'drapeau true';
-				if($_POST['mdp2'] == $_POST['mdp']){
-				
-				$verifMail = $bdd -> prepare('select email from utilisateurs where email= ?');
-				$mailPara = $_POST['mail'];
-				
-				$verifMail -> execute([$mailPara]);
-				
-				if($verifMail -> fetch()){
-					echo "adresse mail deja existente merci de la modifier ou de vous connecter avec le compte correspondant &agrave; l'adresse mail renseign&eacute;e";
-					echo '<meta http-equiv="refresh" content="3; url= nouveau.php?n='.$_POST['ps'].'&mail='.$_POST['mail'].'">';
+			if($_POST['mdp2'] == $_POST['mdp']){
 
-				}else {
-
-					session_start();
-					echo '<meta http-equiv="refresh" content="10; url=Profil.php">';
-
-					echo $_POST['ps']."</br>";
-					echo $_POST['mail']."</br>";
-					echo $_POST['mdp']."</br>";
-
-
-					enregistrer($_POST['ps'], $_POST['mail'], $_POST['mdp']);
+				if($drap){
+					echo 'drapeau true';
 					
-					echo "voici les informations que vous avez rentrée, vous serez redirigé vers la page d'accueil d'ici quelques secondes"; 
-					echo "<br>";
-					echo "Pseudo : ".$_POST['ps'];
-					echo "<br>";
-					echo "Adresse mail : ".$_POST['mail'];
-
-
-					$resultT = $bdd -> prepare('select * from utilisateurs where pseudo = ?');
-
-					$resultT -> execute([$_POST['ps']]);
+					$verifMail = $bdd -> prepare('select email from utilisateurs where email= ?');
+					$mailPara = $_POST['mail'];
 					
-					$result = $resultT  -> fetch();
+					$verifMail -> execute([$mailPara]);
 					
-					$_SESSION['utilisateur'] = array(
-						'utilisateur' => $result['id_utilisateur'],
-						'pseudo' => $result['pseudo'],
-						'MDP' => $result['mot_de_passe'],
-						'email' => $result['email'],
-						'photo' => $result['photo'],
-						'nb_heures' => $result['nb_heures']);
+					if($verifMail -> fetch()){
+						echo "adresse mail deja existente merci de la modifier ou de vous connecter avec le compte correspondant &agrave; l'adresse mail renseign&eacute;e";
+						echo '<meta http-equiv="refresh" content="3; url= nouveau.php?n='.$_POST['ps'].'&mail='.$_POST['mail'].'">';
 
-					echo "test";
-					echo $_SESSION['utilisateur']['utilisateur'];
+					}else {
+						$verifPseudo = $bdd -> prepare('select pseudo from utilisateurs where pseudo= ?');
+						$PsPara = $_POST['ps'];
+						
+						$verifPseudo -> execute([$PsPara]);			
+						if($verifPseudo -> fetch()){
+							echo "Pseudo d&eacute;j&agrave; existent merci de le modifier";
+							echo '<meta http-equiv="refresh" content="3; url= nouveau.php?n='.$_POST['ps'].'&mail='.$_POST['mail'].'">';
 
-					}
+						}else{
+							
+							session_start();
+							echo '<meta http-equiv="refresh" content="3; url=Profil.php">';
 
-					
-					
-					
+							echo $_POST['ps']."</br>";
+							echo $_POST['mail']."</br>";
+							echo $_POST['mdp']."</br>";
 
+
+							enregistrer($_POST['ps'], $_POST['mail'], $_POST['mdp']);
+							
+							echo "voici les informations que vous avez rentrée, vous serez redirigé vers la page d'accueil d'ici quelques secondes"; 
+							echo "<br>";
+							echo "Pseudo : ".$_POST['ps'];
+							echo "<br>";
+							echo "Adresse mail : ".$_POST['mail'];
+
+
+							$resultT = $bdd -> prepare('select * from utilisateurs where pseudo = ?');
+
+							$resultT -> execute([$_POST['ps']]);
+							
+							$result = $resultT  -> fetch();
+							
+							$_SESSION['utilisateur'] = array(
+								'utilisateur' => $result['id_utilisateur'],
+								'pseudo' => $result['pseudo'],
+								'MDP' => $result['mot_de_passe'],
+								'email' => $result['email'],
+								'photo' => $result['photo'],
+								'nb_heures' => $result['nb_heures']);
+
+							echo "test";
+							echo $_SESSION['utilisateur']['utilisateur'];
+							}
+						}
 
 			}else{
-
-				echo "Les mots de passe entrés ne sont pas les mêmes. Vous serez redirigés dans quelques instant";
+				echo 'drapeau false';
+				echo "L'adresse mail que vous avez entr&eacute;e ne contient pas de @. Elle n'est donc pas valide. Merci d'entrer une adresse mail valide.";
 				echo "<br>";
+				
 				echo '<meta http-equiv="refresh" content="3; url= nouveau.php?n='.$_POST['ps'].'&mail='.$_POST['mail'].'">';
 
 			}
 		}else{
-			echo 'drapeau false';
-			echo "L'adresse mail que vous avez entr&eacute;e ne contient pas de @. Elle n'est donc pas valide. Merci d'entrer une adresse mail valide.";
+			echo "Les mots de passe entrés ne sont pas les mêmes. Vous serez redirigés dans quelques instant";
 			echo "<br>";
-			
 			echo '<meta http-equiv="refresh" content="3; url= nouveau.php?n='.$_POST['ps'].'&mail='.$_POST['mail'].'">';
 		}
 	}
